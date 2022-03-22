f8df2a3c3389aef1a4343c43fec11540a4b6b9bf
AnthonyR <arankin789@gmail.com>
2022-03-22 11:56:58 -0400

    redid JS, generates password succesfully

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,58 @@
 // Assignment code here
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+{}|:?><[]";
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = alphaLower.toUpperCase();
 
+
+
+function generatePassword(){
+  
+  // Clears password 
+  var password = "";
+
+
+  // Password variables
+  var passwordLength = prompt("How long would you like your password to be? Password must be between 8-128 characters.");
+  var confirmUpper = confirm("Would you like your password to contain Upper case letters?");
+  var confirmLower = confirm("Would you like your password to contain lower case letters?");
+  var confirmNumber = confirm ("Would you like your password to contain numbers?");
+  var confirmSymbols = confirm("Would you like your password to contain symbols?");
+
+  var passwordText = document.querySelector("#password");
+
+  // passwordLength = parseInt(passwordLength);
+
+  if (passwordLength < 8 || passwordLength > 128){
+    window.alert("Password must be betten 8 and 128 characters.")
+    return"";
+  }
+  for(var i = 0; i < passwordLength; i++){
+
+    if(confirmUpper && password.length < passwordLength){
+      password = password += alphaUpper.charAt(Math.floor(Math.random() * alphaUpper.length));
+    }
+
+    if(confirmLower && password.length < passwordLength){
+      password = password += alphaLower.charAt(Math.floor(Math.random() * alphaLower.length));
+    }
+
+    if(confirmNumber && password.length < passwordLength){
+      password = password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+
+    if(confirmSymbols && password.length < passwordLength){
+      password = password += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    }
+
+    passwordText.value = password;
+
+  }
+
+  console.log(password);
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,8 +62,8 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  // passwordText.value = password;
+  
 }
 
 // Add event listener to generate button
